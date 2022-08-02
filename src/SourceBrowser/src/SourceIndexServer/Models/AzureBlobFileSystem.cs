f0358119fc866eb369e16d4bede14c8825baf7ef
Michael Stuckey <Michael.Stuckey@microsoft.com>
2022-08-02 16:55:59 -0700

    Use released version of Azure.Storage.Blobs

--- src/SourceBrowser/src/SourceIndexServer/Models/AzureBlobFileSystem.cs
+++ src/SourceBrowser/src/SourceIndexServer/Models/AzureBlobFileSystem.cs
@@ -1,46 +1,18 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using Azure.Core.Pipeline;
-using Azure.Storage;
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
 
 namespace Microsoft.SourceBrowser.SourceIndexServer.Models
 {
     public class AzureBlobFileSystem : IFileSystem
     {
-        private class AnonContainerClient : BlobContainerClient
-        {
-            public override Uri Uri { get; }
-            protected override HttpPipeline Pipeline { get; }
-
-            public AnonContainerClient(string uri)
-            {
-                Uri = new Uri(uri);
-                Pipeline = HttpPipelineBuilder.Build(new BlobClientOptions());
-            }
-
-            public override BlobClient GetBlobClient(string blobName)
-            {
-                return (BlobClient)typeof(BlobClient).GetTypeInfo().DeclaredConstructors.Single(ctor => ctor.ToString() == "Void .ctor(System.Uri, Azure.Core.Pipeline.HttpPipeline)").Invoke(new object[] { AppendToPath(this.Uri, blobName), this.Pipeline });
-            }
-
-            private static Uri AppendToPath(Uri uri, string segment)
-            {
-                UriBuilder uriBuilder = new UriBuilder(uri);
-                string path = uriBuilder.Path;
-                uriBuilder.Path = uriBuilder.Path + (path.Length == 0 || path[path.Length - 1] != '/' ? "/" : "") + segment;
-                return uriBuilder.Uri;
-            }
-        }
-
         private readonly BlobContainerClient container;
+
         public AzureBlobFileSystem(string uri)
         {
-            container = new AnonContainerClient(uri);
+            container = new BlobContainerClient(new Uri(uri));
         }
 
         public bool DirectoryExists(string name)
@@ -57,25 +29,18 @@ namespace Microsoft.SourceBrowser.SourceIndexServer.Models
                 dirName += "/";
             }
 
-            return container.GetBlobsByHierarchy("/", new GetBlobsOptions
-            {
-                Prefix = dirName,
-            }).Select(res => res.Value).Where(item => item.IsBlob).Select(item => item.Blob.Name).ToList();
+            return container.GetBlobsByHierarchy(prefix: dirName)
+                .Where(item => item.IsBlob)
+                .Select(item => item.Blob.Name)
+                .ToList();
         }
 
         public bool FileExists(string name)
         {
             name = name.ToLowerInvariant();
             var blob = container.GetBlobClient(name);
-            try
-            {
-                blob.GetProperties();
-                return true;
-            }
-            catch (StorageRequestFailedException ex) when (string.Equals(ex.ErrorCode, "BlobNotFound"))
-            {
-                return false;
-            }
+            
+            return blob.Exists();
         }
 
         public Stream OpenSequentialReadStream(string name)
