0916f3463f159ba5d6049f482698dd158b1f57f0
Ankit Jain <radical@gmail.com>
2024-05-30 16:46:39 -0400

    Avoid using sas tokens for uploading index to the final storage
    .. container.
    
    Instead, use the already logged in managed identity to upload the index
    using the `AzureFileCopy@6` azdo task.
    
    I tried to use `azcopy`, `az storage blob sync`, and `az storage azcopy blob
    sync` but none of them worked as they do not seem to support managed
    identity.
    
    This commit should be paired with turning off key-based access for the
    storage.

--- deployment/upload-index-to-container.ps1
+++ deployment/upload-index-to-container.ps1
@@ -1,32 +1,18 @@
 param(
   [string]$StorageAccountName,
-  [string]$IndexSource,
   [string]$OutFile
 )
 
-
 $ErrorActionPreference = "Stop"
 Import-Module $PSScriptRoot/util.ps1
-
-$StorageAccountKey = Get-StorageAccountKey -StorageAccountName $StorageAccountName
 
 $newContainerName = "index-$((New-Guid).ToString("N"))"
 
 Write-Host "Creating new container '$newContainerName'..."
 {
-  az storage container create --name "$newContainerName" --auth-mode key --public-access container --fail-on-exist --account-name $StorageAccountName --account-key "$StorageAccountKey"
+  az storage container create --name "$newContainerName" --auth-mode login --public-access container --fail-on-exist --account-name $StorageAccountName
 } | Check-Failure
 
-Write-Host "Generating sas-token for container..."
-{
-  $script:sas = az storage container generate-sas --name "$newContainerName" --account-name $StorageAccountName --account-key "$StorageAccountKey" --auth-mode key --permissions cadlrw --expiry $((Get-Date).ToUniversalTime().AddDays(1).ToString("yyyy-MM-ddTHH:mm:ssZ")) | convertfrom-json
-} | Check-Failure
-
-Write-Host "Copying index files into container with azcopy..."
-{
-  azcopy.exe sync $IndexSource "https://$StorageAccountName.blob.core.windows.net/$newContainerName/?$sas" --delete-destination true --log-level ERROR
-} | Check-Failure
-
-Write-Output "Uploaded index to 'https://$StorageAccountName.blob.core.windows.net/$newContainerName'"
+Write-Output "##vso[task.setvariable variable=NEW_CONTAINER_NAME]$newContainerName"
 
 "https://$StorageAccountName.blob.core.windows.net/$newContainerName" | Out-File $OutFile
