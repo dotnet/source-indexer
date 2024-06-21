0c5190d411b6feb1d3811fea48c222121d3db29c
Jo Shields <directhex@apebox.org>
2024-06-21 14:43:19 -0400

    Use Managed Identity in app service to access storage account (#149)
    * port Azure auth code from stage 1 uploader to web service
    
    * forcibly use a client id from environment

--- src/SourceBrowser/src/SourceIndexServer/Models/AzureBlobFileSystem.cs
+++ src/SourceBrowser/src/SourceIndexServer/Models/AzureBlobFileSystem.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,10 +10,21 @@ namespace Microsoft.SourceBrowser.SourceIndexServer.Models
     public class AzureBlobFileSystem : IFileSystem
     {
         private readonly BlobContainerClient container;
+        private DefaultAzureCredential credential;
+        private string clientId;
 
         public AzureBlobFileSystem(string uri)
         {
-            container = new BlobContainerClient(new Uri(uri));
+            if (string.IsNullOrEmpty(clientId) && !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ARM_CLIENT_ID")))
+                clientId = Environment.GetEnvironmentVariable("ARM_CLIENT_ID");
+
+            if (string.IsNullOrEmpty(clientId))
+                credential = new DefaultAzureCredential();
+            else
+                credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = clientId });
+
+            container = new BlobContainerClient(new Uri(uri),
+                                                credential);
         }
 
         public bool DirectoryExists(string name)
