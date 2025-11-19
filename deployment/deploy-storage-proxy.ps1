c4243771745492e565da64d15c123647ac953f91
Ankit Jain <radical@gmail.com>
2025-11-19 16:55:29 -0500

    deploy-storage-proxy.ps1: misc improvements

--- deployment/deploy-storage-proxy.ps1
+++ deployment/deploy-storage-proxy.ps1
@@ -1,6 +1,7 @@
 param(
-  [string]$ProxyUrlFile,
+  [string]$NewContainerName,
   [string]$ResourceGroup,
+  [string]$StorageAccountName,
   [string]$WebappName,
   [string]$Slot
 )
@@ -10,7 +11,26 @@ param(
 $ErrorActionPreference = "Stop"
 Import-Module $PSScriptRoot/util.ps1
 
-$proxyUrl = Get-Content -Raw $ProxyUrlFile
+# validate arguments
+if ([string]::IsNullOrEmpty($NewContainerName)) {
+    throw "NewContainerName is null or empty"
+}
+if ([string]::IsNullOrEmpty($ResourceGroup)) {
+    throw "ResourceGroup is null or empty"
+}
+if ([string]::IsNullOrEmpty($StorageAccountName)) {
+    throw "StorageAccountName is null or empty"
+}
+if ([string]::IsNullOrEmpty($WebappName)) {
+    throw "WebappName is null or empty"
+}
+if ([string]::IsNullOrEmpty($Slot)) {
+    throw "Slot is null or empty"
+}
+
+$proxyUrl = "https://$StorageAccountName.blob.core.windows.net/$NewContainerName"
+
+Write-Host "Setting SOURCE_BROWSER_INDEX_PROXY_URL to: '$proxyUrl'"
 
 {
   az webapp config appsettings set --resource-group $ResourceGroup --name $WebappName --slot $Slot --settings "SOURCE_BROWSER_INDEX_PROXY_URL=$proxyUrl"
