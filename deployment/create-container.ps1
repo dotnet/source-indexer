15545241d04f32df12fdf0cd74c0c440848e10cd
Ankit Jain <radical@gmail.com>
2025-12-08 13:03:09 -0500

    Add missing script (#227)

---
+++ deployment/create-container.ps1
@@ -0,0 +1,15 @@
+param(
+  [string]$StorageAccountName
+)
+
+$ErrorActionPreference = "Stop"
+Import-Module $PSScriptRoot/util.ps1
+
+$newContainerName = "index-$((New-Guid).ToString("N"))"
+
+Write-Host "Creating new container '$newContainerName'..."
+{
+  az storage container create --name "$newContainerName" --auth-mode login --public-access off --fail-on-exist --account-name $StorageAccountName
+} | Check-Failure
+
+Write-Output "##vso[task.setvariable variable=NEW_CONTAINER_NAME]$newContainerName"
